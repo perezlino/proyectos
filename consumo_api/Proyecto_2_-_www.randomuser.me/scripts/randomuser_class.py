4be14a610e9a7fefce39357061947fda066b36c3
Alfonso Perez <perezlino@gmail.com>
2024-05-18 23:08:31 -0400

    Actualizacion directorio C:\github\proyectos\consumo_api\Proyecto_2_-_www.randomuser.me

--- consumo_api/Proyecto_2_-_www.randomuser.me/scripts/randomuser_class.py
+++ consumo_api/Proyecto_2_-_www.randomuser.me/scripts/randomuser_class.py
@@ -9,12 +9,9 @@ class RandomUser_Api():
         pass
 
     logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(funcName)s - %(levelname)s - %(message)s',  
-                        filename='logging.log',                                                
-                        filemode='a') 
+                        format='%(asctime)s - %(funcName)s - %(levelname)s - %(message)s') 
                 
-    def get_random_user(self):                               
-        logging.debug('Entramos a la función get_random_user')                   
+    def obtener_random_user(self):                                                
                                                                                             
         try:                                                                                 
             response = requests.get(f'https://randomuser.me/api/')
@@ -23,28 +20,36 @@ class RandomUser_Api():
             #return json.loads(response.text.encode("utf-8"))
             return json.loads(response.text)    
         except Exception as exception_message:
-            logging.warning(f'No fue posible obtener una respuesta: {exception_message}') 
+            logging.warning(f'No fue posible obtener una respuesta debido a: {exception_message}') 
         return None
 
     def devolverDataframe(self):
-            response = self.get_random_user()
-            #data = json.loads(response.text.encode("utf-8"))
+        try:
+            response = self.obtener_random_user()
+            #respuesta = json.loads(response.text.encode("utf-8"))
             respuesta = response['results'][0]               
             df = pd.json_normalize({'firstname':respuesta['name']['first'],
                                     'lastname':respuesta['name']['last'],
                                     'country':respuesta['location']['country'],
                                     'username':respuesta['login']['username'],
                                     'password':respuesta['login']['password'],
-                                    'email':respuesta['email']})             
+                                    'email':respuesta['email']}) 
+            logging.info('Se creó el Dataframe exitosamente')             
             return df
+        except Exception as exception_message:
+            logging.warning(f'No fue posible crear el Dataframe debido a: {exception_message}')
+        return None
 
     def exportarCSV(self):
-        self.devolverDataframe().to_csv("random_user_c.csv", index=False)    
+
+        try:
+            self.devolverDataframe().to_csv("random_user.csv", index=False)
+            logging.info('Se exportó el Dataframe exitosamente')
+        except Exception as exception_message:
+            logging.warning(f'No fue posible exportar el Dataframe a CSV debido a: {exception_message}')
+        return None  
 
 if __name__ == '__main__':
 
     response = RandomUser_Api()
-    
-    logging.debug('Obtenemos una respuesta')  
-
     logging.info(response.get_random_user())  
